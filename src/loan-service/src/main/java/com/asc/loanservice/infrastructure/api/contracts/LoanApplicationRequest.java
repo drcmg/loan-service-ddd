848f329110712dc125be3726c2a745483782eb6c
Cyprian Grunwald <cpr.grund@gmail.com>
2020-08-07 20:44:22 +0200

    add constraints to LoanApplicationRequest

--- src/loan-service/src/main/java/com/asc/loanservice/infrastructure/api/contracts/LoanApplicationRequest.java
+++ src/loan-service/src/main/java/com/asc/loanservice/infrastructure/api/contracts/LoanApplicationRequest.java
@@ -1,23 +1,44 @@
 package com.asc.loanservice.infrastructure.api.contracts;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import static com.asc.loanservice.infrastructure.constant.Constants.DATE_FORMAT;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoanApplicationRequest {
+
+    @NotNull
+    @NotBlank
     private String customerName;
+    @NotNull
+    @Past
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT)
     private LocalDate customerBirthday;
+    @NotNull
+    @NotBlank
     private String customerTaxId;
+    @NotNull
+    @Positive
     private BigDecimal customerMonthlyIncome;
+    @NotNull
+    @Positive
     private BigDecimal loanAmount;
+    @NotNull
+    @Positive
     private Integer numberOfInstallments;
+    @NotNull
+    @Future
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT)
     private LocalDate firstInstallmentDate;
 }
---
+++ src/loan-service/src/main/java/com/asc/loanservice/infrastructure/constant/Constants.java
@@ -0,0 +1,5 @@
+package com.asc.loanservice.infrastructure.constant;
+
+public final class Constants {
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+}
