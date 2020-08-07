f364e39dd7a723a0b209ad4d0e50c9d62a4e193e
Cyprian Grunwald <cpr.grund@gmail.com>
2020-08-08 01:27:33 +0200

    implement CustomerAgeRule

--- src/loan-service/src/main/java/com/asc/loanservice/domain/loan/rule/CustomerAgeRule.java
+++ src/loan-service/src/main/java/com/asc/loanservice/domain/loan/rule/CustomerAgeRule.java
@@ -2,10 +2,21 @@ package com.asc.loanservice.domain.loan.rule;
 
 import com.asc.loanservice.infrastructure.api.contract.LoanApplicationRequest;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class CustomerAgeRule implements Rule {
 
     @Override
     public Boolean isValid(LoanApplicationRequest loanApplicationRequest) {
-        return true;
+
+        LocalDate customerBirthday = loanApplicationRequest.getCustomerBirthday();
+        LocalDate firstInstallmentDate = loanApplicationRequest.getFirstInstallmentDate();
+        Integer numberOfInstallments = loanApplicationRequest.getNumberOfInstallments();
+
+        LocalDate loanMaturityDate = firstInstallmentDate.plusMonths(numberOfInstallments);
+        Period period = Period.between(customerBirthday, loanMaturityDate);
+
+        return period.getYears() <= 65;
     }
 }
